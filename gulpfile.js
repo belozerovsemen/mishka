e693dc1068a5890cef9197202d18c95c5641d52d
Семен <belozerovsemen@yandex.ru>
2020-05-01 18:50:36 +0500

    Настроил gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+});
+
