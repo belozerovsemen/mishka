e46beb744a422fdbc12c5c31e43c9b185a8644c8
Semen <belozerovsemen@yandex.ru>
2020-06-29 17:44:28 +0500

    Добавил автопрефиксер

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*style.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 });
@@ -27,4 +29,3 @@ gulp.task('watch', function () {
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 });
-
